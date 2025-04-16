998a3b16fd2d87b7250c503ea6880d8e14a19971
Yuri Fernandes <yuriinfo15@gmail.com>
2025-04-15 22:48:52 -0300

    Register User

---
+++ app/Http/Controllers/UsersController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+use App\Models\User;
+
+class UsersController extends Controller
+{
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|unique:users',
+            'password' => 'required|string|min:6',
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        return response()->json(['message' => 'Usuário registrado com sucesso!']);
+    }
+}
